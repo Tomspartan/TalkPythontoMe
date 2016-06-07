514dd232770e2e04f6cb616b37546cb9d9e73c54
tomcummings <tomcummings@gmail.com>
2016-06-07 10:28:09 -0400

    finished app 2

--- 02-GuesstheNumber/program.py
+++ 02-GuesstheNumber/program.py
@@ -7,6 +7,16 @@ print()
 
 the_number = random.randint(0, 100)
 
-guess_text = input('Guess a number between 0 and 100: ')
-guess = int(guess_text)
+guess = -1
+name = input('what is your name?')
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+    if guess < the_number:
+        print('Sorry {}, your guess of {} was too LOW'.format(name, guess))
+    elif guess > the_number:
+        print('Sorry {}, your guess of {} was too HIGH'.format(name, guess))
+    else:
+        print('Good work {}, your guess of {} was correct'.format(name, guess))
 
+print('done')
